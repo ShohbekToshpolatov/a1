2195eeebdd3fac16b0482171071fefc94f4bea11
Shohbek Toshpo'latov <toshpolatovshohbek18@gmail.com>
2024-12-20 11:28:05 +0500

    hammasi bitta repositoryga joylandi

---
+++ 17.dek.py
@@ -0,0 +1,42 @@
+# 1-masala
+'''
+matn = "john McClane"
+print(" ".join(matn.split()[::-1]))
+'''
+# 2-masala
+'''
+matn = "word"
+print(matn.capitalize())
+'''
+
+# 3-masala
+'''
+n = int(input("Son: "))
+for i in range(1, 11):
+    print(f"{n} * {i} = {n * i}")
+'''
+
+# 4-masala
+'''
+def say_hello(name, city, state):
+    return "Hello, {}! Welcome to {}, {}!".format(" ".join(name), city, state) 
+
+print(say_hello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+'''
+
+# 5-masala
+'''
+matn = "Hi!!!"
+print(matn[:-1] if matn.endswith('!') else matn)
+'''
+
+
+
+
+
+
+
+
+
+
+
